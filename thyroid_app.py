4bade1de1202186342e2dd0217189e3bb216e370
aanyam10 <aanyamendapara@gmail.com>
2025-01-18 22:47:53 -0500

    Update thyroid_app.py

--- thyroid_app.py
+++ thyroid_app.py
@@ -41,26 +41,26 @@ def main():
     st.number_input("**Age:**", min_value=0, max_value=120, step=1)
 
     # Feature: Stage (I: 0, II: 1, III: 2, IVB: 3, IVA: 4)
-    stage = st.selectbox("Stage:", options=[0, 1, 2, 3, 4], format_func=lambda x: {
+    stage = st.selectbox("**Stage:**", options=[0, 1, 2, 3, 4], format_func=lambda x: {
         0: "I", 1: "II", 2: "III", 3: "IVB", 4: "IVA"
     }[x])
 
     # Feature: T (Tumor size) (T1a: 0, T1b: 1, T2: 2, T3a: 3, T3b: 4, T4a: 5, T4b: 6)
-    t = st.selectbox("T (Tumor size):", options=[0, 1, 2, 3, 4, 5, 6], format_func=lambda x: {
+    t = st.selectbox("**T (Tumor size):**", options=[0, 1, 2, 3, 4, 5, 6], format_func=lambda x: {
         0: "T1a", 1: "T1b", 2: "T2", 3: "T3a", 4: "T3b", 5: "T4a", 6: "T4b"
     }[x])
 
     # Feature: N (Node involvement) (N0: 0, N1b: 1, N1a: 2)
-    n = st.selectbox("N (Node involvement):", options=[0, 1, 2], format_func=lambda x: {
+    n = st.selectbox("**N (Node involvement):**", options=[0, 1, 2], format_func=lambda x: {
         0: "N0", 1: "N1b", 2: "N1a"
     }[x])
 
     # Feature: Adenopathy ('No':0,'Right':1,'Extensive':2,'Left':3,'Bilateral':4,'Posterior':5)
-    adenopathy = st.selectbox("Adenopathy:", options=[0, 1, 2, 3, 4, 5], format_func=lambda x: {
+    adenopathy = st.selectbox("**Adenopathy:**", options=[0, 1, 2, 3, 4, 5], format_func=lambda x: {
         0: "No", 1: "Right", 2: "Extensive", 3: "Left", 4: "Bilateral", 5: "Posterior"}[x])
 
     # Feature: Response ('Excellent':0,'Indeterminate':1,'Structural Incomplete':2,'Biochemical Incomplete':3)
-    response = st.selectbox("Response:", options=[0, 1, 2, 3], format_func=lambda x: {
+    response = st.selectbox("**Response:**", options=[0, 1, 2, 3], format_func=lambda x: {
         0: "Excellent", 1: "Indeterminate", 2: "Structural Incomplete", 3: "Biochemical Incomplete"}[x])
 
     # Combine all features into a single input array
